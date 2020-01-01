0c4ed114f0312d99955014d646c19f05ee27b460
sam38124 <sam38124@gmail.com>
2020-01-01 16:05:58 +0800

    Initial Commit

---
+++ JzIos_Framework/JzIos_Framework.h
@@ -0,0 +1,19 @@
+//
+//  JzIos_Framework.h
+//  JzIos_Framework
+//
+//  Created by 王建智 on 2020/1/1.
+//  Copyright © 2020 王建智. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JzIos_Framework.
+FOUNDATION_EXPORT double JzIos_FrameworkVersionNumber;
+
+//! Project version string for JzIos_Framework.
+FOUNDATION_EXPORT const unsigned char JzIos_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JzIos_Framework/PublicHeader.h>
+
+
